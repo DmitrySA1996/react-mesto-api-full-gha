167595c83775e49444f1dc1fbb6d013e49bae01f
Dmitry Senchenko <114693652+DmitrySA1996@users.noreply.github.com>
2023-07-07 06:36:21 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const routes = require('./src/routes/router');
+const limiter = require('./src/middlewares/rateLimiter');
+const errorHandler = require('./src/middlewares/errorHandler');
+
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const app = express();
+
+const { requestLogger, errorLogger } = require('./src/middlewares/logger');
+
+app.use(helmet());
+app.disable('x-powered-by');
+app.use(express.json());
+
+app.use(requestLogger);
+
+app.use(routes);
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log('БД подключена');
+  })
+  .catch(() => {
+    console.log('Не удалось подключиться к БД');
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(limiter);
+
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
+
+app.listen(PORT);
